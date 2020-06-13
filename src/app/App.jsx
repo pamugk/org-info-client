2acb8a91360497a59a1dc00c6e088da85c067a74
Alexander Permyakov <pamugk@gmail.com>
2020-06-13 22:30:52 +0500

    Implemented pretty decent navbar

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,42 +1,60 @@
-import React from 'react';
-import { Switch, Route, Link } from "react-router-dom";
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import { Switch, Route, NavLink, Redirect } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
 
 import AppBar from '@material-ui/core/AppBar';
-import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 
-import EmployeeCreator from '../pages/EmployeeCreator'
+import EmployeeCreator from '../pages/EmployeeCreator';
 import EmployeeList from '../pages/EmployeeList';
 import EmployeeTree from '../pages/EmployeeTree';
-import OrganizationCreator from '../pages/OrganizationCreator'
+import OrganizationCreator from '../pages/OrganizationCreator';
 import OrganizationList from '../pages/OrganizationList';
 import OrganizationTree from '../pages/OrganizationTree';
+import { Toolbar } from '@material-ui/core';
 
-const theme = createMuiTheme({
-  overrides: {
-    MuiAppBar: {
-      root: {
-        flexDirection: "row"
-      }
-    }
+const useStyles = makeStyles((theme) => ({
+  createBtn: {
+    marginLeft: "auto"
   }
-});
+}));
 
 const App = () => {
+  const classes = useStyles();
+  const [createBtnState, setCreationBtnState] = useState(0);
+  const [showCreateBtn, isShowingCreateBtn] = useState(true);
+
+  const hideCreationBtn = () => isShowingCreateBtn(false);
+
+  const setEmpCreation = () => {
+    setCreationBtnState(0);
+    isShowingCreateBtn(true);
+  }
+  const setOrgCreation = () => {
+    setCreationBtnState(1);
+    isShowingCreateBtn(true);
+  }
+
+  const selectCreationLink = () => {
+    switch(createBtnState) {
+      case 0: return "/employees/add";
+      case 1: return "/organizations/add";
+      default: return "#";
+    };
+  }
+
   return (
-    <ThemeProvider theme={theme}>
-      <Box display="flex" flexDirection="column" flexGrow="1">
+    <Box display="flex" flexDirection="column" flexGrow="1">
         <AppBar position="sticky">
-          <Tabs>
-            <Tab label="Список сотрудников" />
-            <Tab label="Список организаций" />
-            <Tab label="Дерево сотрудников" />
-            <Tab label="Дерево организаций" />
-          </Tabs>
-          <Button color="inherit">Создать</Button>
+          <Toolbar>
+            <Button color="inherit" component={NavLink} onClick={setEmpCreation} to="/employees/list">Список сотрудников</Button>
+            <Button color="inherit" component={NavLink} onClick={setOrgCreation} to="/organizations/list">Список организаций</Button>
+            <Button color="inherit" component={NavLink} onClick={hideCreationBtn} to="/employees/tree">Дерево сотрудников</Button>
+            <Button color="inherit" component={NavLink} onClick={hideCreationBtn} to="/organizations/tree">Дерево организаций</Button>
+            { showCreateBtn ? <Button classes={{ root: classes.createBtn}} color="inherit" component={NavLink} to={selectCreationLink()}>Создать</Button> : null  }
+            
+          </Toolbar>
         </AppBar>
         <Switch>
           <Route path="/employees/add">
@@ -57,9 +75,9 @@ const App = () => {
           <Route path="/organizations/tree">
             <OrganizationTree />
           </Route>
+          <Redirect from="/" to="/employees/list"/>
         </Switch>
       </Box>
-    </ThemeProvider>
   );
 }
 
